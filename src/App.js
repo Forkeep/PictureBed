0dcd5e0044bd0897be2bb952c70fe5b805d42648
Forkeep <13126593533@163.com>
2020-06-30 22:37:03 +0800

    lazy import

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from "./components/Header";
-import Home from "./views/home";
-import About from "./views/about";
-import History from "./views/history";
 import {Switch, Route} from 'react-router-dom';
+import Loading from "./components/Loading";
+// import Loading from "./components/Loading";
+
+
+// import Home from "./views/home";
+// import About from "./views/about";
+// import History from "./views/history";
+const Home = lazy(() => import('./views/home.js'));
+const About = lazy(() => import('./views/about.js'));
+const History = lazy(() => import('./views/history.js'));
+
+console.log(Loading);
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/history' component={History}/>
-        <Route path='/about' component={About} />
-      </Switch>
+      <Suspense fallback={Loading()}>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/history' component={History}/>
+          <Route path='/about' component={About}/>
+        </Switch>
+      </Suspense>
       <Footer/>
     </div>
   );
